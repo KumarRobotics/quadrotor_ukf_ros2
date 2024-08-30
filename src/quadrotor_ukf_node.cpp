9805db65217d2b997ee458d15045954aef716cac
kash <kashishgarg9@gmail.com>
2024-08-30 13:17:10 -0400

    Saving point for home

--- src/quadrotor_ukf_node.cpp
+++ src/quadrotor_ukf_node.cpp
@@ -1,7 +1,7 @@
 #include "rclcpp/rclcpp.hpp"
-#include "sensor_msgs/msg/imu.hpp"  // Change this to the actual message type you'll use for each callback
 #include "nav_msgs/msg/odometry.hpp"  // Change this to the actual message type you'll use for each callback
-#include "geometry_msgs/msg/posestamped.hpp"  // Change this to the actual message type you'll use for each callback
+#include "sensor_msgs/msg/imu.hpp"  // Change this to the actual message type you'll use for each callback
+#include "geometry_msgs/msg/pose_stamped.hpp"  // Change this to the actual message type you'll use for each callback
 
 class QuadrotorUKFNode : public rclcpp::Node
 {
@@ -19,35 +19,38 @@ public:
         pose_to_tf_subscriber_ = this->create_subscription<geometry_msgs::msg::PoseStamped>(
             "/qvio/pose", 10, std::bind(&QuadrotorUKFNode::pose_to_tf_callback, this, std::placeholders::_1));
     }
+    void imu_callback(const sensor_msgs::msg::Imu::UniquePtr msg);
+    void vio_callback(const nav_msgs::msg::Odometry::UniquePtr msg);
+    void pose_to_tf_callback(const geometry_msgs::msg::PoseStamped::UniquePtr pose);
 
 private:
-    // Callback for VIO data
-    void vio_callback(const nav_msgs::msg::Odometry::SharedPtr msg)
-    {
-        RCLCPP_INFO(this->get_logger(), "Received VIO data");
-        
-        // TODO: Add VIO data processing for UKF
-    }
-
-    // Callback for IMU data
-    void imu_callback(const sensor_msgs::msg::Imu::SharedPtr msg)
-    {
-        RCLCPP_INFO(this->get_logger(), "Received IMU data");
-        // TODO: Add IMU data processing for UKF
-    }
-
-    // Callback for pose to TF data
-    void pose_to_tf_callback(const geometry_msgs::msg::PoseStamped::SharedPtr msg)
-    {
-        RCLCPP_INFO(this->get_logger(), "Received pose to TF data");
-        // TODO: Convert pose to TF and publish it
-    }
 
     // Subscribers
     rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr vio_subscriber_;
     rclcpp::Subscription<sensor_msgs::msg::Imu>::SharedPtr imu_subscriber_;
     rclcpp::Subscription<geometry_msgs::msg::PoseStamped>::SharedPtr pose_to_tf_subscriber_;
 };
+
+// Callback for IMU data
+void QuadrotorUKFNode::imu_callback(const sensor_msgs::msg::Imu::UniquePtr msg)
+{
+    RCLCPP_INFO(this->get_logger(), "Received IMU data");
+    // TODO: Add IMU data processing for UKF
+}
+
+// Callback for VIO data
+void QuadrotorUKFNode::vio_callback(const nav_msgs::msg::Odometry::UniquePtr msg)
+{
+    RCLCPP_INFO(this->get_logger(), "Received VIO data");
+    // TODO: Add VIO data processing for UKF
+}
+
+// Callback for pose to TF data
+void QuadrotorUKFNode::pose_to_tf_callback(const geometry_msgs::msg::PoseStamped::UniquePtr msg)
+{
+    RCLCPP_INFO(this->get_logger(), "Received pose to TF data");
+    // TODO: Convert pose to TF and publish it
+}
 
 int main(int argc, char *argv[])
 {
