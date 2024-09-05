ae8107da292f6f96c771e072823d643ff47cb696
kash <kashishgarg9@gmail.com>
2024-09-05 15:36:14 -0400

    Working bag, working build/run and fixed up run docker script

--- run_ros2_docker.sh
+++ run_ros2_docker.sh
@@ -17,6 +17,7 @@ docker run -it \
     --env="DISPLAY" \
     --env="QT_X11_NO_MITSHM=1" \
     --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
+    --network host \
     ros2 
     #rqt
 export containerId=$(docker ps -l -q)
